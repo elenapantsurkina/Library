29cb8ff8ecc99c926dab60898d51d9502c721186
elena_pantsurkina <pantsurkina.e@bk.ru>
2024-10-22 13:45:02 +0700

    lesson 24

--- library/urls.py
+++ library/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
-from .views import books_detail, books_list
+# from .views import books_detail, books_list
+from .views import BooksListView, BookCreateView, BookDetailView, BookUpdateView, BookDeleteView
 
 app_name = 'library'
 
 urlpatterns = [
-    path('books_list/', books_list, name='books_list'),
-    path('books_detail/<int:book_id>', books_detail, name='books_detail'),
+    path('books/', BooksListView.as_view(), name='books_list'),
+    path('books/<int:pk>/', BookDetailView.as_view(), name='books_detail'),
+    path('books/new/', BookCreateView.as_view(), name='book_create'),
+    path('books/update/<int:pk>/', BookUpdateView.as_view(), name='book_update'),
+    path('books/delete/<int:pk>/', BookDeleteView.as_view(), name='book_delete'),
+    # path('books_list/', books_list, name='books_list'),
+    # path('books_detail/<int:book_id>', books_detail, name='books_detail'),
 
 ]
